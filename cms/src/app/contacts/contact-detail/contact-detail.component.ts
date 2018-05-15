2410f253c6e01e7c58e35fed1d72f4ea70fdc62f
ell12025 <ell12025@byui.edu>
2018-05-15 16:59:07 -0600

    5/15/2018 - header directory added and contact-list and contact-details showing in contact component

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from "../contact.model";
 
 @Component({
   selector: 'app-contact-detail',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent implements OnInit {
+  contacts: Contact[] = [
+    new Contact('1', 'Bro. Jackson', 'jacksonk@byui.edu', '208-496-3771',
+      'http://www.byui.edu/images/instructional-development/News/stories/06-2015/jacksonk.jpg', 'null'),
 
+    new Contact('2', 'Bro. Barzee', 'barzeer@byui.edu','208-496-3768',
+      'https://images-na.ssl-images-amazon.com/images/I/51TXuDJxNYL._UX250_.jpg', 'null')
+  ];
   constructor() { }
 
   ngOnInit() {
