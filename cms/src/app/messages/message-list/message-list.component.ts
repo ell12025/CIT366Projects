a439bae0bfe2ae323f480dc921cf5a935db7b7e5
ell12025 <ell12025@byui.edu>
2018-07-11 23:07:55 -0600

    7/11/2018 - Lesson 08 in progress. Setting up Messages Service...

--- cms/src/app/messages/message-list/message-list.component.ts
+++ cms/src/app/messages/message-list/message-list.component.ts
@@ -1,23 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import { Message } from "../message.model";
 import { MessageService } from "../message.service";
+import { Subscription } from "rxjs/internal/Subscription";
 
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.css']
 })
-export class MessageListComponent implements OnInit {
+export class MessageListComponent implements OnInit, OnDestroy {
   messages: Message[] = [];
+  subscription: Subscription;
 
 
   constructor(private messageService: MessageService) { }
 
   ngOnInit() {
-    this.messages = this.messageService.getMessages();
-    this.messageService.messageChangeEvent.subscribe((messages: Message[]) => {
+    this.messageService.getMessages();
+    this.subscription = this.messageService.messageChangeEvent.subscribe(
+      (messages: Message[]) => {
       this.messages = messages;
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 
--- cms/src/app/messages/message.service.ts
+++ cms/src/app/messages/message.service.ts
@@ -1,36 +1,85 @@
-import {EventEmitter, Injectable} from '@angular/core';
-
+import { EventEmitter, Injectable } from '@angular/core';
 import { Message } from "./message.model";
-
-import { MOCKMESSAGES } from "./MOCKMESSAGES";
+import { HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable()
 export class MessageService {
   messageChangeEvent = new EventEmitter<Message[]>();
   messages: Message[] = [];
+  maxMessageId: number;
 
-  getMessages() {
-    return this.messages.slice();
+
+  // Class constructor
+  constructor(private http: HttpClient) {
+    this.getMessages();
+    this.maxMessageId = this.getMaxId();
+
   }
 
-  getMessage(id: string) {
+
+  // Get messages
+  getMessages() {
+    // return this.messages.slice();
+
+    this.http.get<Message[]>('https://cit366-c75e2.firebaseio.com/messages.json').subscribe(
+      (messages: Message[]) => {
+        this.messages = messages;
+        this.maxMessageId = this.getMaxId();
+        this.messageChangeEvent.emit(this.messages.slice());
+      },
+      (error: any) => {
+        console.log("HTTP error: " + error.msgText);
+      }
+    );
+  }
+
+
+  // Save messages
+  storeMessages() {
+    const strMessages = JSON.stringify(this.messages);
+    const header = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.put('https://cit366-c75e2.firebaseio.com/messages.json',
+      strMessages,
+      {headers: header}).subscribe(
+      () => this.messageChangeEvent.emit(this.messages.slice()));
+  }
+
+  // getMessage(id: string) {
+  //   for(var i = 0; i < this.messages.length; i++)
+  //   {
+  //     if(this.messages[i].id == id)
+  //     {
+  //       return this.messages[i];
+  //     }
+  //   }
+  //   return null;
+  // }
+
+
+  // Get max ID
+  getMaxId():number {
+    var maxId = 0;
+
     for(var i = 0; i < this.messages.length; i++)
     {
-      if(this.messages[i].id == id)
+      var currentId = parseInt(this.messages[i].id);
+      if(currentId > maxId)
       {
-        return this.messages[i];
+        maxId = currentId;
       }
     }
-    return null;
+    return maxId;
   }
 
+
+  // Add a message
   addMessage(message: Message) {
     this.messages.push(message);
-    this.messageChangeEvent.emit(this.messages.slice());
+    // this.messageChangeEvent.emit(this.messages.slice());
+    this.storeMessages();
   }
 
-
-  constructor() {
-    this.messages = MOCKMESSAGES;
-  }
 }
